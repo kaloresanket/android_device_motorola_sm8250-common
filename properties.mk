ccbb6412be6195cabb34832b49ab8f5d8b8baf73
Midas Chien <midaschieh@google.com>
2021-10-30 17:48:22 +0000

    sm8250-common: Don't latch unsignal buffers
    Set debug.sf.latch_unsignaled=1 is used to allow surfaceflinger to
    latch unsignal buffers that can give more time for buffer to render.
    But if the buffer takes longer time to finish, it will block whole
    display pipeline result to jank.
    
    Bug: 157964883
    Test: scrolling, launch
    Change-Id: I56c3e05921de5fc9569bf3f93fde03022a64b6a4

--- properties.mk
+++ properties.mk
@@ -384,7 +384,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.sf.native_mode=2 \
     debug.sf.hw=0 \
     debug.egl.hw=0 \
-    debug.sf.latch_unsignaled=1 \
     debug.sf.high_fps_late_app_phase_offset_ns=1000000 \
     debug.mdpcomp.logs=0 \
     vendor.gralloc.disable_ubwc=0 \
