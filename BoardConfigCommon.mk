4e21221847e0d710ef0160dbcb2ef08cde19aa84
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 13:22:44 -0500

    sm8250-common: Remove elf files in copy files check
    New extract files uses packages instead of copy files
    
    Change-Id: I337c07d13bd350958d4808e1693cc6e03753a96c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,14 +48,12 @@ AB_OTA_PARTITIONS += \
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
-# BUILD_BROKEN_*
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
 # Camera
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket
