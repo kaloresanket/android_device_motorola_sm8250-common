1ac6acc15f86e3a4078cb68d13777790c7d30871
kaloresanket <kaloresanket@gmail.com>
2024-03-29 07:00:17 +0000

    Revert "sm8250-common: Add ViPER4Android FX support"
    This reverts commit f2056cb8e7ae282ca042fe8252681d35f2353cea.

--- common.mk
+++ common.mk
@@ -484,23 +484,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
-<<<<<<< HEAD
-=======
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
-
-# Vibrator
-$(call inherit-product, hardware/xiaomi/aidl/vibrator/vibrator-vendor-product.mk)
-
-# Viper
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
-
->>>>>>> f1d71765... sm8350-common: Add ViPER4Android FX support
 # VNDK
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := strict
 
