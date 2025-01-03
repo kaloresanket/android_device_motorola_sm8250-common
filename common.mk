b163a2e210d09651c2474897ec2f51115b4f7d71
bxySo <bxyso@outlook.com>
2025-01-04 00:34:07 +0800

    sm8250-common: rro_overlays: Support A/B OTA perf mode
    * This would improve OTA.
    
    Co-authored-by: Cyber Knight <cyberknight755@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- common.mk
+++ common.mk
@@ -65,8 +65,18 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=erofs \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 PRODUCT_PACKAGES += \
+    checkpoint_gc \
     otapreopt_script
+
+PRODUCT_PACKAGES += \
+    OpenDeltaOverlayKona
 
 # Partitions
 PRODUCT_PACKAGES += \
